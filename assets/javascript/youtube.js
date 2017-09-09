ef20c098b07350832cca02eaba999ac5d0a3bf53
alexjr1194 <alexjr1194@me.com>
2017-09-09 10:45:36 -0700

    made youtube give me responses

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,25 +1,27 @@
- function keyWordsearch(){
-        gapi.client.setApiKey('AIzaSyCj4YymhzXyuUv5yC88T2EQ_apysoUeUmg');
-        gapi.client.load('youtube', 'v3', function() {
-                makeRequest();
-        });
-}
-    function makeRequest() {
-        var q = $('#query').val();
-        var request = gapi.client.youtube.search.list({
-                q: q,
-                part: 'snippet', 
-                maxResults: 10
-        });
-        request.execute(function(response)  {                                                                                    
-                $('#results').empty()
-                var srchItems = response.result.items;                
-                $.each(srchItems, function(index, item) {
-                vidTitle = item.snippet.title;  
-                vidThumburl =  item.snippet.thumbnails.default.url;
-                vidID = "video ID : " + item.id.videoId             
-                vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image Available." style="width:204px;height:128px"></pre>';                   
+$(function() {
+    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&"
+    var key = "AIzaSyCj4YymhzXyuUv5yC88T2EQ_apysoUeUmg"
+    var q = "";
 
-                $('#results').append('<pre>' + vidTitle + vidThumbimg + vidID +'</pre>');                      
-        })  
-    }) 
+    $("#search-button").on("click", function(event) {
+        event.preventDefault();
+        search();
+    })
+
+    function search() {
+        q = $("#query").val();
+        console.log(q)
+
+        $.get(
+            "https://www.googleapis.com/youtube/v3/search",{
+                part:'snippet',
+                q: q,
+                type: 'video',
+                key: 'AIzaSyCj4YymhzXyuUv5yC88T2EQ_apysoUeUmg'
+            }, function(data){
+                console.log(data)
+                
+            }
+        );
+    };
+})
