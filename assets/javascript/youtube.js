a724b61fd4f339a1fbe2667a8111c5d9f2978284
alexjr1194 <alexjr1194@me.com>
2017-09-09 11:14:24 -0700

    got youtube to work

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,6 +1,4 @@
 $(function() {
-    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&"
-    var key = "AIzaSyCj4YymhzXyuUv5yC88T2EQ_apysoUeUmg"
     var q = "";
 
     $("#search-button").on("click", function(event) {
@@ -9,19 +7,59 @@ $(function() {
     })
 
     function search() {
+
+        $("#results").html("");
+
+
+
         q = $("#query").val();
         console.log(q)
 
         $.get(
             "https://www.googleapis.com/youtube/v3/search",{
-                part:'snippet',
+                part:'snippet, id',
                 q: q,
                 type: 'video',
                 key: 'AIzaSyCj4YymhzXyuUv5yC88T2EQ_apysoUeUmg'
             }, function(data){
+
                 console.log(data)
-                
+
+                $.each(data.items, function(i, item) {
+                    var output = getOutput(item);
+                    $("#results").append(output);
+
+
+
+
+
+                })
             }
         );
     };
+
+    function getOutput(item) {
+        var videoId = item.id.videoId;
+        var title = item.snippet.title;
+        var description = item.snippet.discription;
+        var thumb = item.snippet.thumbnails.high.url;
+        var channelTitle = item.snippet.channelTitle;
+        var videoDate = item.snippet.publishedAt;
+
+
+
+        var output = '<li>' +
+        '<div class="list-left">' +
+        '<img src="' + thumb + '">' +
+        '</div>' +
+        '<div class="list-right">'+
+        '<h3>' + title+ '</h3>'+
+        '<small>By <span class="cTitle">' +channelTitle+ '</span> on '+videoDate+'</small>'+
+        '<p>'+description+'</p>'+
+        '</div>'+
+        '</li>'+
+        '<div class="clearFix"></div>'+ ''
+        ;
+        return output
+    }
 })
