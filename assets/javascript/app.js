79f13250f5e73937fbe539cb4eeed9737a6d8dc0
Akunna Ogbu <AkunnaOgbu@calvisitor-10-105-140-222.calvisitor.1918.berkeley.edu>
2017-09-12 17:51:39 -0700

    initial

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+<!Doctype html>
+<body>
+
+
+<script>
+ $(document).ready(function() {
+
+  var placesAutocomplete = places({
+    container: document.querySelector('#where')
+    
+  });
+</script>
+<body>
+</html>
+
+
